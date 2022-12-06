39a8a0ef4b31f96726707971c496a1407af5b8a9
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2022-12-06 21:28:57 +0300

    console.log the temp infos with selected city

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 // https://api.openweathermap.org/data/2.5/weather?q=London&APPID=17ea5ba9a9dc62a9c0de1045c75d8170
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Istanbul&APPID=17ea5ba9a9dc62a9c0de1045c75d8170', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response);
-        console.log('temperature :' + response.main.temp + ' but it feels like :' + response.main.feels_like);
-        console.log('it is expected to see highest as ' + response.main.temp_max + ' and lowest as ' + response.main.temp_min);
-        console.log('Sky has ' + response.weather[0].description);
-        console.log('end of the weather fetch')
-    })
+
 
 // https://gist.githubusercontent.com/ozdemirburak/4821a26db048cc0972c1beee48a408de/raw/4754e5f9d09dade2e6c461d7e960e13ef38eaa88/cities_of_turkey.json
 
@@ -30,10 +20,7 @@ fetch('https://gist.githubusercontent.com/ozdemirburak/4821a26db048cc0972c1beee4
 });
 
 const selectMaker = () => {
-    const select = document.createElement('select');
-    select.classList.add('city-selector');
-    select.setAttribute('id', 'city-options')
-    containerDiv.appendChild(select);
+    const select = document.querySelector('#city-options')
 
     for(let index = 0; index < cityList.length; index++){
         const option = document.createElement('option');
@@ -41,5 +28,25 @@ const selectMaker = () => {
         option.setAttribute('value', cityList[index]);
         option.textContent = `${cityList[index]}`
         select.appendChild(option);
+
     }
 };
+
+const selectOpt = document.querySelector('#city-options');
+
+selectOpt.addEventListener('change', () => {
+    console.log(selectOpt.value)
+
+//    https://api.openweathermap.org/data/2.5/weather?q=London&APPID=17ea5ba9a9dc62a9c0de1045c75d8170
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectOpt.value}&APPID=17ea5ba9a9dc62a9c0de1045c75d8170`, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            console.log(response);
+            console.log('temperature :' + response.main.temp + ' but it feels like :' + response.main.feels_like);
+            console.log('it is expected to see highest as ' + response.main.temp_max + ' and lowest as ' + response.main.temp_min);
+            console.log('Sky has ' + response.weather[0].description);
+            console.log('end of the weather fetch')
+        })
+})
