e949d1551f53146e237caec61505ee9fead7d2d3
Ozan Ayyıldız <ozanayyildiz92@gmail.com>
2022-12-06 21:11:16 +0300

    no changes made

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=Istanbul&APPID=17ea5ba9
         console.log('temperature :' + response.main.temp + ' but it feels like :' + response.main.feels_like);
         console.log('it is expected to see highest as ' + response.main.temp_max + ' and lowest as ' + response.main.temp_min);
         console.log('Sky has ' + response.weather[0].description);
+        console.log('end of the weather fetch')
     })
 
 // https://gist.githubusercontent.com/ozdemirburak/4821a26db048cc0972c1beee48a408de/raw/4754e5f9d09dade2e6c461d7e960e13ef38eaa88/cities_of_turkey.json
@@ -22,7 +23,6 @@ fetch('https://gist.githubusercontent.com/ozdemirburak/4821a26db048cc0972c1beee4
     return citynames.json();
 })
 .then(function(citynames) {
-    console.log(citynames);
     for(let index = 0; index < citynames.length; index++){
         cityList.push(citynames[index].name);
     }
@@ -32,12 +32,14 @@ fetch('https://gist.githubusercontent.com/ozdemirburak/4821a26db048cc0972c1beee4
 const selectMaker = () => {
     const select = document.createElement('select');
     select.classList.add('city-selector');
+    select.setAttribute('id', 'city-options')
     containerDiv.appendChild(select);
 
     for(let index = 0; index < cityList.length; index++){
-    const option = document.createElement('option');
-    option.classList.add('options');
-    option.textContent = `${cityList[index]}`
-    select.appendChild(option);
+        const option = document.createElement('option');
+        option.classList.add('options');
+        option.setAttribute('value', cityList[index]);
+        option.textContent = `${cityList[index]}`
+        select.appendChild(option);
     }
 };
